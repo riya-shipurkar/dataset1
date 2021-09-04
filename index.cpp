9f716a657aa72913ab71878d6a2ac568f790686b
Riya Shipurkar <riyashipurkar14@gmail.com>
2021-09-04 13:07:46 -0700

    Initial commit

---
+++ index.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+  string negative, positive;
+  int num;
+
+cout << " Line 4.\t with rage, yelled quotes to me.";
+cout << "\t -1" << endl;
+getline(cin, negative)
+cout << "Line 8. \t all else is dreaery chaos!";
+cout << "\t -1" << endl;
+getline(cin, negative)
+cout << "Line 19.\t 'Knowledge is power!'";
+cout << "\t +1" << endl;
+getline(cin, positive)
+cout << "Line 26.\t excellent constitution of body";
+cout << "\t +1" << endl;
+getline(cin, positive)
+cout << "Line 28.\t pleasing voice";
+cout << "\t +1" << endl;
+getline(cin, positive)
+
+
+
+
+  return 0;
+}
---
+++ index2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+include<fstream>
+using namespace std;
+
+int main()
+{
+  ifstream inputFile;
+  inputFile.open(index.cpp);
+  string negative, positive;
+  index.cpp
+
+  if (positive > negative)
+  {
+    cout << "Output is positive";
+  }
+else if (negative > positive) {
+  cout << "Output is negative";
+}
+else if (positive == negative)
+{
+  cout << "The text is neutral";
+}
+
+
+
+  return 0;
+}
